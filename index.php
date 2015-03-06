917985c9cf85b793384f28f26c06307d10db9eb8
Daniel Hill <dan@joystickinteractive.com>
2015-03-05 22:41:45 -0800

    adding

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<?php
-
-require("lib/vendor/OpenTok/OpenTok.php");
-
-$apiKey = 14125532;
-$apiSecret = "6bd69c1953ba422cb7fab5fe2b5769d8f077ac1d";
-
-$opentok = new OpenTok($apiKey, $apiSecret);
-
-$session = $opentok->createSession();
-
-
-?>
-
 <html>
   <head>
 
@@ -39,18 +25,39 @@ $session = $opentok->createSession();
     <div id='subscribersDiv'></div>
 
     <script>
+
+
+    $.ajax({
+      type: "POST",
+      headers: { 'X-TB-PARTNER-AUTH': '14125532:6bd69c1953ba422cb7fab5fe2b5769d8f077ac1d' }
+      url: "https://api.opentok.com/session/create"
+    }).done(function( msg ) {
+          console.log(msg);
+    });
+
+
+
+
+     /*
       var apiKey = '14125041';
       var sessionId = '1_MX4xNDEyNTA0MX5-MTQyMDY5MzA1NDU0N341a09ZV2VEUzU4WEJsVFRsL0RwN1RhQlB-fg';
       var token = 'T1==cGFydG5lcl9pZD0xNDEyNTA0MSZzaWc9MzU3NWE3MzBmYmFkNTI1N2RkZjllOTU1NDkxMGJhODcyNWY2MDU1ZTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0eE5ERXlOVEEwTVg1LU1UUXlNRFk1TXpBMU5EVTBOMzQxYTA5WlYyVkVVelU0V0VKc1ZGUnNMMFJ3TjFSaFFsQi1mZyZjcmVhdGVfdGltZT0xNDI1NjEzMTM5Jm5vbmNlPTAuMTIyODI1NDcyMzYyOTMzMTc=';
       var session = OT.initSession(apiKey);
 
       console.log(session);
+*/
 
-      var fbref =
+
+
+
+      var fbref = new Firebase("https://seshroulette.firebaseio.com");
       fbref.child("streams").on("value",listStreams);
       fbref.child("streams").push({
           "sessionID":sessionId
       });
+
+
+/*
 
       session.on({
           //streamCreated: function(event) {
@@ -65,7 +72,7 @@ $session = $opentok->createSession();
         }
       });
 
-
+*/
       function listStreams(e){
         $("#streams").empty();
         var streams = e.val();
