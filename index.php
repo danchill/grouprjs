c4d57e02ba3c1c9a593ccf1e692761e1cd915b33
Daniel Hill <dan@joystickinteractive.com>
2015-03-05 21:33:38 -0800

    fixing path

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     $API_SECRET = "704e3a4b2082783f11a2856c63bf8750c89bee61";
 
     include 'lib/vendor/OpenTok/OpenTok.php';
-    /*
+
     $apiObj = new OpenTok($API_KEY, $API_SECRET) or die("couldnt make opentok");
+
+    /*
 
     var_dump($apiObj);
 
