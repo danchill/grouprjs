35220bd0dbe2457039c353b18cb5851f850b3240
Daniel Hill <dan@joystickinteractive.com>
2015-03-05 21:29:19 -0800

    fixing path

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     $API_KEY = 14125041;
     $API_SECRET = "704e3a4b2082783f11a2856c63bf8750c89bee61";
 
-    echo $API_KEY;
-
     require_once 'lib/vendor/php/OpenTok/OpenTok.php';
 
     $apiObj = new OpenTok($API_KEY, $API_SECRET);
+
+    var_dump($apiObj);
+
     $session = $apiObj->create_session($_SERVER["REMOTE_ADDR"],
         array('mediaMode' => MediaMode::ROUTED));
     $session = $apiObj->create_session();
