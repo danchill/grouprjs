9fe9c4d004b2946416119047ca50027817575723
Daniel Hill <dan@joystickinteractive.com>
2015-03-05 21:51:43 -0800

    adding all

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+    require_once 'lib/php/vendor/OpenTok/OpenTok.php';
+
+    $apiObj = new OpenTok($API_KEY, $API_SECRET);
+    $session = $apiObj->create_session($_SERVER["REMOTE_ADDR"],
+        array('mediaMode' => MediaMode::ROUTED));
+    $session = $apiObj->create_session();
+    //echo $session->getSessionId();
+?>
+
 <html>
   <head></head>
   <body>
@@ -8,12 +19,9 @@
     <script src='//static.opentok.com/webrtc/v2.2/js/opentok.min.js'></script>
     <script>
       var apiKey = '14125041';
-      var sessionId = '<?php echo $session->getSessionId(); ?>';
+      var sessionId = '1_MX4xNDEyNTA0MX5-MTQyMDY5MzA1NDU0N341a09ZV2VEUzU4WEJsVFRsL0RwN1RhQlB-fg';
       var token = 'T1==cGFydG5lcl9pZD0xNDEyNTA0MSZzaWc9MzU3NWE3MzBmYmFkNTI1N2RkZjllOTU1NDkxMGJhODcyNWY2MDU1ZTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0eE5ERXlOVEEwTVg1LU1UUXlNRFk1TXpBMU5EVTBOMzQxYTA5WlYyVkVVelU0V0VKc1ZGUnNMMFJ3TjFSaFFsQi1mZyZjcmVhdGVfdGltZT0xNDI1NjEzMTM5Jm5vbmNlPTAuMTIyODI1NDcyMzYyOTMzMTc=';
       var session = OT.initSession(apiKey, sessionId);
-
-      console.log(session);
-
       session.on({
           streamCreated: function(event) {
             session.subscribe(event.stream, 'subscribersDiv', {insertMode: 'append'});
