c511f155cf7728a335ea5ddf444756e0f7e93adb
Daniel Hill <dan@joystickinteractive.com>
2015-03-05 22:13:28 -0800

    adding all

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?php
-
-    require_once 'lib/vendor/OpenTok/OpenTok.php';
-
-
-?>
-
 <html>
   <head>
 
@@ -19,27 +12,11 @@
     }
 
     </style>
+
+
+    <script src='//static.opentok.com/webrtc/v2.2/js/opentok.min.js'></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdn.firebase.com/js/client/2.2.2/firebase.js"></script>
-
-    <script>
-
-
-
-
-     function listStreams(e){
-
-       $("#streams").empty();
-
-       var streamIDs = e.val();
-
-       for(x in streamIDs){
-         $("#streams").append(streamIDs[x]+"<br />");
-       }
-
-     }
-
-    </script>
 
   </head>
   <body>
@@ -47,18 +24,14 @@
     <div id='myPublisherDiv'></div>
     <div id='subscribersDiv'></div>
 
-    <script src='//static.opentok.com/webrtc/v2.2/js/opentok.min.js'></script>
     <script>
       var apiKey = '14125041';
       var sessionId = '1_MX4xNDEyNTA0MX5-MTQyMDY5MzA1NDU0N341a09ZV2VEUzU4WEJsVFRsL0RwN1RhQlB-fg';
       var token = 'T1==cGFydG5lcl9pZD0xNDEyNTA0MSZzaWc9MzU3NWE3MzBmYmFkNTI1N2RkZjllOTU1NDkxMGJhODcyNWY2MDU1ZTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0eE5ERXlOVEEwTVg1LU1UUXlNRFk1TXpBMU5EVTBOMzQxYTA5WlYyVkVVelU0V0VKc1ZGUnNMMFJ3TjFSaFFsQi1mZyZjcmVhdGVfdGltZT0xNDI1NjEzMTM5Jm5vbmNlPTAuMTIyODI1NDcyMzYyOTMzMTc=';
       var session = OT.initSession(apiKey, sessionId);
 
-      var streamsRef = new Firebase("https://seshroulette.firebaseio.com/");
-
-      streamsRef.on("value",listStreams);
-
-      streamsRef.push(session.sessionID);
+      var fbref = new Firebase("http://seshroulette.firebaseio.com");
+      fbref.child("streams").push("sdfasdfasdfdf");
 
       session.on({
           //streamCreated: function(event) {
